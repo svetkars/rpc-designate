51bd4b61907ba0babed9f8e5778ec703ecc4f62a
John Ward <jpward1981@gmail.com>
2018-03-07 09:30:39 -0600

    RI-136: Update for pike

--- gating/scripts/pre.sh
+++ gating/scripts/pre.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright 2014-2017 , Rackspace US, Inc.
+# Copyright 2014-2018, Rackspace US, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,8 +16,9 @@
 ## Shell Opts ----------------------------------------------------------------
 set -xeuo pipefail
 
-## Vars ----------------------------------------------------------------------
+## Vars and Functions --------------------------------------------------------
 source ${PWD}/gating/scripts/vars.sh
+source ${PWD}/gating/scripts/functions.sh
 
 ## Main ----------------------------------------------------------------------
 echo "Pre gate job started"
@@ -25,52 +26,29 @@ echo "+-------------------- START ENV VARS --------------------+"
 env
 echo "+-------------------- START ENV VARS --------------------+"
 
-# If RE_JOB_SCENARIO is set to functional, we need to change it to newton
-if [ ${RE_JOB_SCENARIO} == 'functional' ]; then
-  export RE_JOB_SCENARIO="newton"
-fi
+# If we're not using a pre-saved rpc-openstack image, then deploy the
+# necessary OpenStack infrastructure and services.
+if [[ ! ${RE_JOB_IMAGE} =~ _snapshot$ ]]; then
 
-# Setup a few variables specific to the scenario that we are running
-case $RE_JOB_SCENARIO in
-"newton")
-  # Pin RPC-Release to 14.3 for newton
-  export RPC_RELEASE="r14.3.0"
-  export RPC_PRODUCT_RELEASE=${RE_JOB_SCENARIO}
-  export OSA_BASE_DIR=${OS_BASE_DIR}/openstack-ansible
+  case $RE_JOB_SCENARIO in
+  "newton")
+    # Pin RPC-Release to 14.3 for newton
+    export RPC_RELEASE="r14.3.0"
+    export RPC_PRODUCT_RELEASE=${RE_JOB_SCENARIO}
+    export OSA_BASE_DIR=${OS_BASE_DIR}/openstack-ansible
+    clone_openstack
+    gate_deploy_newton
   ;;
-"pike")
-  # Right now since we don't have the pike code included, just exit
-  exit
-esac
-
-# Clone rpc-openstack
-if [ ! -d ${OS_BASE_DIR} ]; then
-  git clone --recursive -b ${RPC_RELEASE} https://github.com/rcbops/rpc-openstack ${OS_BASE_DIR}
+  "pike")
+    export RPC_RELEASE="r16.0.0"
+    export RPC_PRODUCT_RELEASE=${RE_JOB_SCENARIO}
+    export OSA_BASE_DIR=${OS_BASE_DIR}/openstack-ansible
+    clone_openstack
+    gate_deploy_pike
+  ;;
+  esac
+  
 fi
- 
-if [ ${RPC_PRODUCT_RELEASE} == "newton" ]; then 
-  echo "python-ldap==2.5.2" >> ${OSA_BASE_DIR}/global-requirement-pins.txt
-  echo "Flask!=0.11,<1.0,>=0.10" >> ${OSA_BASE_DIR}/global-requirement-pins.txt
-fi
-
-# Make sure that we are in the base dir and deploy openstack aio
-cd ${OS_BASE_DIR}
-#${OS_BASE_DIR}/scripts/deploy.sh
-# Setup ansible for the environment
-scripts/bootstrap-ansible.sh
-
-# Setup the aio environment
-scripts/bootstrap-aio.sh
-
-cd ${OS_BASE_DIR}/openstack-ansible
-# Configure Host for openstack
-openstack-ansible playbooks/setup-hosts.yml
-
-# Configure Infrastructure for Openstack
-openstack-ansible playbooks/setup-infrastructure.yml
-
-# Keystone is the only openstack service that we need installed
-openstack-ansible playbooks/os-keystone-install.yml
 
 # Install Designate
 cd ${MY_BASE_DIR}
